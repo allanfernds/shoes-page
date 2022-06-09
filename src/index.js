3383ee14b19ac7240e81cc9c93dd85aa6898135f
Alan Fernandes <61387305+allanfernds@users.noreply.github.com>
2022-06-09 05:06:41 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+
+
+function alterarImagem01(){
+    document.getElementById("mainImage").setAttribute("src", "images/image-product-1.jpg")
+}
+
+
+function alterarImagem02(){
+    document.getElementById("mainImage").setAttribute("src", "images/image-product-2.jpg")
+}
+
+
+function alterarImagem03(){
+    document.getElementById("mainImage").setAttribute("src", "images/image-product-3.jpg")
+}
+
+
+function alterarImagem04(){
+    document.getElementById("mainImage").setAttribute("src", "images/image-product-4.jpg")
+}
